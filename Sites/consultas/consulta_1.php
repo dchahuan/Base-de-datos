6ee068c10e803dc4ad2048721773529a2f677202
dchahuan <d.chahuan@uc.cl>
2020-09-22 19:39:08 -0300

    a

--- Sites/consultas/consulta_1.php
+++ Sites/consultas/consulta_1.php
@@ -47,10 +47,37 @@ $nombres = $result -> fetchAll();
 
             <div class="mx-auto mt-3">
                 <h1>Consulta 1</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
                 <?php
-                echo $nombres[0][1];
-                echo $nombres[1][0];
-                echo sizeOf($nombre[0]);
+                
                 ?>
             </div>
 
