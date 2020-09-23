16543ec555b05d9ecb2453782ab0df85f1113e89
dchahuan <d.chahuan@uc.cl>
2020-09-23 15:03:49 -0300

    a

--- Sites/components/sidebar.php
+++ Sites/components/sidebar.php
@@ -9,58 +9,58 @@
 </head>
 
 <body>
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse  vh-100 position-sticky>
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse  vh-100 position-sticky">
         <div class=" sidebar-sticky pt-3">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/index.php">
-                    Index
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/esquemas.php">
-                    Esquema
-                </a>
-            </li>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/index.php">
+                        Index
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/esquemas.php">
+                        Esquema
+                    </a>
+                </li>
 
-        </ul>
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-2 mb-0 text-dark">
-            <span>Consultas</span>
-        </h6>
-        <div class="dropdown-divider"></div>
+            </ul>
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-2 mb-0 text-dark">
+                <span>Consultas</span>
+            </h6>
+            <div class="dropdown-divider"></div>
 
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_1.php">
-                    Consulta 1
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_2.php">
-                    Consulta 2
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_3.php">
-                    Consulta 3
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_4.php">
-                    Consulta 4
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_5.php">
-                    Consulta 5
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_6.php">
-                    Consulta 6
-                </a>
-            </li>
-        </ul>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_1.php">
+                        Consulta 1
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_2.php">
+                        Consulta 2
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_3.php">
+                        Consulta 3
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_4.php">
+                        Consulta 4
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_5.php">
+                        Consulta 5
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="/~grupo16/consultas/consulta_6.php">
+                        Consulta 6
+                    </a>
+                </li>
+            </ul>
         </div>
     </nav>
 </body>
