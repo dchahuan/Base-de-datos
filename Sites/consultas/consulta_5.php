60884816caceaea1d2d246c0f13b40826106efca
dchahuan <d.chahuan@uc.cl>
2020-09-30 18:34:51 -0300

    5

--- Sites/consultas/consulta_5.php
+++ Sites/consultas/consulta_5.php
@@ -1,9 +1,6 @@
 <?php
 require("../config/conexion.php");
-$query = "select * from personal where pasaporte in (select distinct pasaporte from buques, capitanes where buques.patente in (select patente from atracos where lower(puerto) like '%talcahuano%') and capitanes.patente = buques.patente)  and genero like 'mujer';";
-$result = $db -> prepare($query);
-$result -> execute();
-$capitanes= $result -> fetchAll();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,40 +48,52 @@ $capitanes= $result -> fetchAll();
                         <input type="text" class="form-control" name="Puerto">
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                            value="option1" checked>
+                        <input class="form-check-input" type="radio" name="genero" id="hombre" value="option1" checked>
                         <label class="form-check-label" for="exampleRadios1">
-                            Default radio
+                            Hombre
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                            value="option2">
+                        <input class="form-check-input" type="radio" name="genero" id="exampleRadios2" value="mujer">
                         <label class="form-check-label" for="exampleRadios2">
                             Second default radio
                         </label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary mt-3">Submit</button>
                 </form>
-                <table class="table table-striped my-3">
-                    <thead>
-                        <tr>
-                            <th scope="col">Pasaporte</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Genero</th>
-                            <th scope="col">Edad</th>
-                            <th scope="col">Nacionalidad</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+                <?php
+                    if (isset($_GET) && array_key_exists('puerto',$_GET) &&  array_key_exists('genero',$_GET)){
+
+                        $query = "select buques.nombre, buques.patente, buques.tipo, buques.bpais from buques where patente in (select patente from atracos where lower(puerto) like lower('%".$_GET['puerto']."%') and fecha_llegada > '".$_GET["ano"]."-1-1' and fecha_llegada < '".$_GET["ano"]."-12-31');";
+                        $result = $db -> prepare($query);
+                        $result -> execute();
+                        $buques = $result -> fetchAll();
+
+                        $query = "select * from personal where pasaporte in (select distinct pasaporte from buques, capitanes where buques.patente in (select patente from atracos where lower(puerto) like lower('".$_GET['puerto']."')) and capitanes.patente = buques.patente)  and genero like '".$_GET['genero']."';";
+                        $result = $db -> prepare($query);
+                        $result -> execute();
+                        $capitanes= $result -> fetchAll();
+                        echo "<table class='table table-striped my-3'>
+                        <thead>
+                            <tr>
+                                <th scope='col'>Pasaporte</th>
+                                <th scope='col'>Nombre</th>
+                                <th scope='col'>Genero</th>
+                                <th scope='col'>Edad</th>
+                                <th scope='col'>Nacionalidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>";
+
                         foreach ($capitanes as $c){
                             echo "<tr><td>$c[0]</td><td>$c[1]</td><td>$c[2]</td><td>$c[3]</td><td>$c[4]</td></tr>";
-                            
-                        }
-                        ?>
-                    </tbody>
-                </table>
+
+                                    }
+
+                                echo "</tbody>
+                            </table>";
+                    }
+                ?>
             </div>
 
 
