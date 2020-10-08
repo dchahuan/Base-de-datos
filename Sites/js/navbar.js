2e02e3bcbd1f31bc6b52cf8da7f065ed52743abe
dchahuan <d.chahuan@uc.cl>
2020-10-08 13:23:44 -0300

    a

--- Sites/js/navbar.js
+++ Sites/js/navbar.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const boton_puerto = document.getElementById("nav-puerto");
 
   boton_naviera.addEventListener("click", () => {
-    if (!("active" in boton_naviera.contains(class_name))) {
+    if (!boton_naviera.classList.contains(class_name)) {
       boton_naviera.classList.add(class_name);
     }
 
