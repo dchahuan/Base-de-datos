1f5ae8d9794a5ec386a9f08b09db03384b3de3a1
dchahuan <d.chahuan@uc.cl>
2020-10-11 12:24:38 -0300

    css

--- Sites/login/signup_form.php
+++ Sites/login/signup_form.php
@@ -5,71 +5,76 @@
 ?>
 
 <body>
-<div class = "bg-dark">
-<div class="container mx-auto rounded border border-dark bg-light   ">
-    <div class="container text-center ">
-    
-    <h1>Signup<h1>
-    
-    </div>
+    <div class="bg-dark vh-100">
+        <div class="container mx-auto rounded border border-dark bg-light   ">
+            <div class="container text-center ">
 
-    <hr style = "border:1px solid black;">
+                <h1>Signup<h1>
 
-    <form action="/login/scripts/singup.php" method="post">
-        <div class = "form">
-            <div class="form-group">
-                <label for="name" class = "col-3 control-label">Nombre: </label>
-                <input type="text" name="name" class = "col-9 form-control" placeholder = "Username" id = "name">
             </div>
-            <div class="form-group">
-                <label for="edad" class = "col-3 control label">Edad: </label>
-                <input type="num" min = "0" step = "1" class = "col-9 form-control"name="edad" placeholder = "Edad" id = "edad">
-            </div>
-            <div class="form-group">
-                <label for="pasaporte" class = "col-3 control-label">Numero de pasaporte: </label>
-                <input type="text" name="pasaporte" class = "col-9 form-control" placeholder = "Numero de pasaporte" id = "pasaporte">
-            </div>
-            <div class="form-group">
-                <label for="nacionalidad" class = "col-3 control-label">Nacionalidad </label>
-                <input type="text" name="nacionalidad" class = "col-9 form-control" placeholder = "Nacionalidad" id = "nacionalidad">
-            </div>
-            <fieldset class="form-group">
-                <div class="row mx-1">
-                    <legend class="col-form-label col-sm-2 pt-0">Genero</legend>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                                <input class="form-check-input" type="radio" name="sexo" id="hombre" value="hombre" checked>
-                                <label class="form-check-label" for="hombre">
-                                    Hombre
-                                </label>
+
+            <hr style="border:1px solid black;">
+
+            <form action="/login/scripts/singup.php" method="post">
+                <div class="form">
+                    <div class="form-group">
+                        <label for="name" class="col-3 control-label">Nombre: </label>
+                        <input type="text" name="name" class="col-9 form-control" placeholder="Username" id="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="edad" class="col-3 control label">Edad: </label>
+                        <input type="num" min="0" step="1" class="col-9 form-control" name="edad" placeholder="Edad"
+                            id="edad">
+                    </div>
+                    <div class="form-group">
+                        <label for="pasaporte" class="col-3 control-label">Numero de pasaporte: </label>
+                        <input type="text" name="pasaporte" class="col-9 form-control" placeholder="Numero de pasaporte"
+                            id="pasaporte">
+                    </div>
+                    <div class="form-group">
+                        <label for="nacionalidad" class="col-3 control-label">Nacionalidad </label>
+                        <input type="text" name="nacionalidad" class="col-9 form-control" placeholder="Nacionalidad"
+                            id="nacionalidad">
+                    </div>
+                    <fieldset class="form-group">
+                        <div class="row mx-1">
+                            <legend class="col-form-label col-sm-2 pt-0">Genero</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="sexo" id="hombre" value="hombre"
+                                        checked>
+                                    <label class="form-check-label" for="hombre">
+                                        Hombre
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="sexo" id="mujer" value="mujer">
+                                    <label class="form-check-label" for="mujer">
+                                        Mujer
+                                    </label>
+                                </div>
                             </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="sexo" id="mujer" value="mujer">
-                                <label class="form-check-label" for="mujer">
-                                    Mujer
-                                </label>
                         </div>
+                    </fieldset>
+
+                    <div class="form-group">
+                        <label for="pwd" class="col-3 control-label">Contraseña:</label>
+                        <input type="password" class="col-9 form-control" name="pwd" placeholder="Password" id="pwd">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd-r" class="col-3 control-label">Repita su contraseña: </label>
+                        <input type="password" class="col-9 form-control" name="pwd-r"
+                            placeholder="Reapeat your password">
                     </div>
                 </div>
-            </fieldset>
 
-            <div class="form-group">
-                <label for="pwd" class = "col-3 control-label">Contraseña:</label>
-                <input type="password" class = "col-9 form-control" name="pwd" placeholder = "Password" id = "pwd">
-            </div>
-            <div class="form-group">
-                <label for="pwd-r" class = "col-3 control-label">Repita su contraseña: </label>
-                <input type="password" class = "col-9 form-control" name="pwd-r" placeholder = "Reapeat your password">
-            </div>
-        </div>
-        
-        
-        <button type="submit" name = "signup-submit" class = "btn btn-dark btn-block ">Register</button>
-        
-        
-    </form>
 
-    <?php
+                <button type="submit" name="signup-submit" class="btn btn-dark btn-block ">Register</button>
+
+
+            </form>
+
+            <?php
         if (isset($_GET["error"])){
             echo "<h7 class = 'text-danger'>";
             if ($_GET["error"] == "emptyfields"){
@@ -85,10 +90,11 @@
         }
     ?>
 
+        </div>
     </div>
-</div>
-    
-    
-    
+
+
+
 </body>
+
 </html>
