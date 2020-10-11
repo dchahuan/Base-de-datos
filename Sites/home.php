e95c59f386e22932adb32d12d5b056a958bef4d9
dchahuan <d.chahuan@uc.cl>
2020-10-11 15:49:58 -0300

    a

--- Sites/home.php
+++ Sites/home.php
@@ -9,7 +9,7 @@ if (isset($_SESSION["pasaporte"])){
     $result = $db -> prepare($query);
     $result -> execute([$_SESSION["pasaporte"]]);
     $data = $result -> fetchAll();
-    print_r(array_keys($data));
+    print_r($data);
 ?>
 
 
