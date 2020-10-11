6284a89f6a9f70d301eb9dcfb776104b0f69a6cc
dchahuan <d.chahuan@uc.cl>
2020-10-11 15:49:02 -0300

    a

--- Sites/home.php
+++ Sites/home.php
@@ -1,9 +1,15 @@
 <?php
-include "components/head.php";
-include "components/header.php";
+    include "components/head.php";
+    include "components/header.php";
+
+
 require("config/conexion.php");
 if (isset($_SESSION["pasaporte"])){
-    echo "hello ". $_SESSION["pasaporte"];
+    $query = "select * from usuarios where pasaporte = ?";
+    $result = $db -> prepare($query);
+    $result -> execute([$_SESSION["pasaporte"]]);
+    $data = $result -> fetchAll();
+    print_r(array_keys($data));
 ?>
 
 
