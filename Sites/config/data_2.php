c319d55ac4e58b1965a8c5a111249b1989009ef8
Jose Feliu <jose_feliu@uc.cl>
2020-10-19 19:04:55 -0300

    puertos añadidos

---
+++ Sites/config/data_2.php
@@ -0,0 +1,5 @@
+<?php
+    $user = 'grupo43';
+    $password = '11032012';
+    $databaseName = 'grupo43e3';
+?>
--- Sites/consultas/puertos.php
+++ Sites/consultas/puertos.php
@@ -1,4 +1,11 @@
 <?php
+    require("../config/conexion_2.php");
+    $query = "SELECT * FROM Puertos";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $puertos = $result -> fetchAll();
+?>
+<?php
     include "../components/head.php"
 ?>
 
@@ -9,6 +16,25 @@
     <div class="container-fluid">
         <div class="text-center">
             <h1>Puertos</h1>
+        </div>
+        <div class="row justify-content-around">
+            <?php
+            foreach($puertos as $p){
+                
+                echo "<div class='card col-xs-12 col-sm-6 col-md-4 col-lg-2 cole-xl-1 mx-2 mt-2' style='width: 18rem;'>
+                <div class='card-body'>
+                  <h5 class='card-title'>$p[0]</h5>
+                  <h6 class='card-subtitle mb-2 text-muted'>$p[1], $p[2], Chile</h6>
+                <form method = 'POST' action = 'naviera.php'>
+                    <input value='$p[0]' name='nid' class = 'd-none' type='text' disable>
+                    <input value='$p[1]' name='name' class = 'd-none' type='text' disable>
+                    <button type='submit' class='btn btn-dark'>Mas info</button>
+                </form>
+                </div>
+              </div>";
+                
+            }
+        ?>
         </div>
 
 
