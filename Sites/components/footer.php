0eb2d8eef490cbde8e204a222fbce4dfd34b2a45
Jose Feliu <jose_feliu@uc.cl>
2020-10-24 13:53:28 -0300

    puertos

--- Sites/components/footer.php
+++ Sites/components/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer" style="position:absolute; bottom:0; width: 100%; height: 2.5rem;">
+<footer class="footer" style="height: 2.5rem;">
     <hr>
     <div class="text-center mb-2">Todas las fotos ocupadas en este proyecto son open source y de <a
             href="https://www.unsplash.com/">unsplash.com<a></div>
