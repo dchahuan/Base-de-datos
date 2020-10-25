555db30411927593a0a059af937a5a43c5b6f7ed
Jose Feliu <jose_feliu@uc.cl>
2020-10-24 21:58:14 -0300

    puertos

--- Sites/consultas/consulta1_puerto.php
+++ Sites/consultas/consulta1_puerto.php
@@ -1,5 +1,13 @@
 <?php
+    function trim_value(&$value) 
+    { 
+        $value = trim($value); 
+    }
     require("../config/conexion_2.php");
+    $fecha_array = explode("-", $_POST['date_range']);
+    array_walk($fecha_array, 'trim_value');
+    $fecha_inicio = $fecha_array[0]; 
+    $fecha_fin = $fecha_array[1];
     // añadir query y procedimiento almacenado
 ?>
 <?php
@@ -21,7 +29,7 @@
             </h1>
             <p>
                 <?php
-                    echo "Rango de fechas seleccionadas: ". $_POST['date_range'];
+                    echo "Rango de fechas seleccionadas: ". $fecha_inicio . "y ".$fecha_fin;
                 ?>
             </p>
         </div>
