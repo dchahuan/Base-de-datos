498efb97567a96d3a103e997f06e3bfafbbf4b31
Jose Feliu <jose_feliu@uc.cl>
2020-10-26 22:34:49 -0300

    puertos consulta 1

--- Sites/consultas/consulta2_puerto.php
+++ Sites/consultas/consulta2_puerto.php
@@ -3,17 +3,22 @@
     $patente_barco = $_POST['patente_barco'];
     $tipo_instalacion = $_POST['tipo_instalacion'];
     $fecha_atraque = $_POST['fecha_atraque'];
+    $fecha_atraque_2 = new DateTime($_POST['fecha_atraque']);
     if ($tipo_instalacion == 'muelle'){
         $fecha_salida = $fecha_atraque;
     } elseif ($tipo_instalacion == 'astillero'){
         $fecha_salida = $_POST['fecha_salida'];
     }
+    $fecha_salida_2 = new DateTime($_POST['fecha_salida']);
     $nombre_puerto = $_POST['nombre_puerto'];
     // añadimos el procedimiento almacenado que retorna una tabla
     $stored_procedure = "SELECT Instalaciones.id_instalacion, dias FROM calcular_capacidad(?, ?, ?), Instalaciones WHERE iid = id_instalacion AND tipo_instalacion = ? ORDER BY id_instalacion";
     $result_procedure = $db_2 -> prepare($stored_procedure);
     $result_procedure -> execute([$nombre_puerto, $fecha_atraque, $fecha_salida, $tipo_instalacion]);
     $capacidad_instalaciones = $result_procedure -> fetchAll();
+    // para añadir a permisos necesito: 
+    // Permisos_Astilleros -> id_instalacion, patente_barco, fecha_atraque, fecha_salida
+    // Permisos_Muelles	-> id_instalacion, patente_barco, fecha_atraque, descripcion_actividad	
 ?>
 <?php
     include "../components/head.php"
@@ -39,7 +44,7 @@
             </p>
             <p>
                 <?php
-                    echo "Se generará un permiso de tipo $tipo_instalacion para la instalación x, diferencia entre fechas: ".$diff->days. 'days';
+                    echo "Se generará un permiso de tipo $tipo_instalacion para la instalación x en las fechas seleccionadas";
                 ?>
             </p>
             <table class="table">
