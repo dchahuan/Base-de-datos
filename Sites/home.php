54de045e13326766c1750dc209404a73b1ca6b13
dchahuan <d.chahuan@uc.cl>
2020-10-27 14:22:34 -0300

    aa

--- Sites/home.php
+++ Sites/home.php
@@ -1,9 +1,9 @@
 <?php
         include "components/head.php";
-        include "components/header.php";
+        
 
 if (isset($_SESSION["pasaporte"])){
-
+    include "components/header.php";
     require("config/conexion.php");
     require("config/conexion_2.php");
 
@@ -214,8 +214,6 @@ if (isset($_SESSION["pasaporte"])){
     }
 
  } else {
-   print_r($_SESSION);
-    #header("Location: /~grupo16/index.php?error=nouser");
-    #exit();
-
+    header("Location: /~grupo16/index.php?error=nouser");
+    exit();
 }?>
