c60722e9d92614f780ee48ce2ed95f11971c17a4
dchahuan <d.chahuan@uc.cl>
2020-11-30 20:40:32 -0300

    testing

--- Sites/mensajes/scripts/funciones_mensajes.php
+++ Sites/mensajes/scripts/funciones_mensajes.php
@@ -3,11 +3,11 @@
 function get_user_id($name){
     $res = file_get_contents("https://entrega5-bdd.herokuapp.com/users");
     $res = json_decode($res);
-
+    print_r($res);
     $uid;
 
     foreach($res as $user){
-        echo $user["name"];
+        print_r($user);
     }
 
     return $uid;
