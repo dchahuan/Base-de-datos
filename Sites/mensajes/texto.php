3edc07c720ad9b88d95c1769ba020cdecb11d259
dchahuan <d.chahuan@uc.cl>
2020-11-30 20:23:06 -0300

    mas problemas de ruta

--- Sites/mensajes/texto.php
+++ Sites/mensajes/texto.php
@@ -3,10 +3,10 @@
         
 
 if (isset($_SESSION["pasaporte"])){
-    include "components/head.php";
-    include "components/header.php";
-    require("config/conexion.php");
-    require("config/conexion_2.php");
+    include "../components/head.php";
+    include "../components/header.php";
+    require("../config/conexion.php");
+    require("../config/conexion_2.php");
 
 
     $query_nombre_ususario = "select nombre from usuarios where pasaporte = ?";
