10ee3c829edec1f91fd67980146f349223d15320
dchahuan <d.chahuan@uc.cl>
2020-11-30 20:47:04 -0300

    encontrar id

--- Sites/mensajes/scripts/funciones_mensajes.php
+++ Sites/mensajes/scripts/funciones_mensajes.php
@@ -4,14 +4,12 @@ function get_user_id($name){
     $res = file_get_contents("https://entrega5-bdd.herokuapp.com/users");
     $res = json_decode($res);
     $uid;
-    print_r($res[0]->name);
-
     foreach($res as $user){
-        
+        if ($user->name == $name){
+            $uid = $user->uid;
+        } 
     }
 
     return $uid;
 }
-
-
 ?>
