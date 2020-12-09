6fdbb9bb8e607c38e9b5d445ec0ea736eb95d176
dchahuan <d.chahuan@uc.cl>
2020-12-09 16:56:22 -0300

    adding map

--- Sites/mapa/mapa.php
+++ Sites/mapa/mapa.php
@@ -16,17 +16,32 @@ if (isset($_SESSION["pasaporte"])){
     $data_nombre = $data_nombre[0];
 ?>
 
-
+<body>
 <div class="container">
     <h3 class = "text-center"> 
         Mapa Mensajes
     </h3>
     
     <div id = "mapid" style = "heigth = 300px"></div>
+
 </div>
 
+<script>
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+  </script>
+
+</body>
+
 <?php
- } else {
+} else {
     header("Location: /~grupo16/index.php?error=nouser");
     exit();
 }?>
