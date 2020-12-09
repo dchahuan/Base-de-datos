2e331878f6c4daaf5cad9c1baf1ed0c33b54b4e9
dchahuan <d.chahuan@uc.cl>
2020-12-09 11:20:01 -0300

    a

--- Sites/mensajes/text_search.php
+++ Sites/mensajes/text_search.php
@@ -26,14 +26,58 @@ if (isset($_SESSION["pasaporte"])){
                 $d = trim($_POST["d"]);
                 $r = trim($_POST["r"]);
                 $f = trim($_POST["f"]);
-                $mensajes = text_search($d,$r,$f,$data_nombre);
+                $array_mesajes = text_search($d,$r,$f,$data_nombre);
+                if (count($array_mesajes) == 0){
+                    echo "<h5>No has enviado mensajes :(</h5>";
+                } else {
+                    $uid_actual = $array_mesajes[0]->receptant;
+                    echo "<div class = 'card mb-2'>";
+                    echo '<div class="card-body">';
+                    echo "<h3>Mensajes enviados a ".get_user_name($uid_actual)."</h3></br>";
+                    $contador = 0;
+                    foreach($array_mesajes as $mensaje){
+                        if ($mensaje->receptant != $uid_actual){
+                            $uid_actual = $mensaje->receptant;
+                            $contador  = 0;
+                            echo "</div>";
+                            echo "</div>";
+                            echo "<div class = 'card'>";
+                            echo '<div class="card-body">';
+                            echo "<h3>Mensajes enviados a ".get_user_name($uid_actual)."</h3></br>";
+                        }
+                        echo "<h5>Mensaje #$contador</h5>";
+                        echo '<table class="table user-view-table m-0">
+                        <tbody>
+                        <tr>
+                            <td>Mensaje</td>
+                            <td>'.$mensaje->message.'</td>
+                        </tr>
+                        <tr>
+                            <td>Latitud:</td>
+                            <td>'.$mensaje->lat.'</td>
+                        </tr>
+                                            
+                        <tr>
+                            <td>Longitud:</td>
+                            <td>'.$mensaje->long.'</td>
+                        </tr>
+                        <tr>
+                            <td>Fecha de emision:</td>
+                            <td>'.$mensaje->date.'</td>
+                        </tr>
+                        </tbody>
+                        </table>';
+                        $contador++;
+                    }
+                    
+                }
+                echo "</div>";
+                echo "</div>";
+                        
+                    } else{
+                        echo "<h3 class = 'text-danger'>Tienes que entrar a este link por el form</h3>";
+                    }
                 
-                print_r($mensajes);
-                
-            } else{
-                echo "<h3 class = 'text-danger'>Tienes que entrar a este link por el form</h3>";
-            }
-        
         ?>
     
     
